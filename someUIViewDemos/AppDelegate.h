14bcc0c53a6586bdee4de45731663a14abdd88c7
Nguyen Thanh To <nguyenthanhto@Nguyens-Mac.local>
2017-07-13 08:40:29 +0700

    Initial Commit

---
+++ someUIViewDemos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  someUIViewDemos
+//
+//  Created by Nguyen Thanh To on 7/13/17.
+//  Copyright © 2017 ThanhTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ someUIViewDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  someUIViewDemos
+//
+//  Created by Nguyen Thanh To on 7/13/17.
+//  Copyright © 2017 ThanhTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
